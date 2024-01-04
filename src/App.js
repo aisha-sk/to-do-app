bd74dc3a9fb476a19a660042b335e49c28c32ecc
aisha-sk <aishasuhail2005@gmail.com>
2024-01-04 12:49:10 +0400

    Add todo component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from './Todo'
+const style = {
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#EBB8DD] to-[#CAE7D3]`
+}
 
 function App() {
+  const [todos,setTodos] = useState(['Learn React', 'Grind LC'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 classname={style.heading}>To Do App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add todo" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index)=>(
+            <Todo key={index} todo={todo}/>
+          ))}
+          
+        </ul>
+        <p classnmae={style.count}>You have 2 todos</p>
+      </div>
     </div>
   );
 }
