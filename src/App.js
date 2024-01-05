f1682da9f0eb238c03574e518cd7d253f6cd46ee
aisha-sk <aishasuhail2005@gmail.com>
2024-01-05 14:49:08 +0400

    Add read method

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo'
 import {db} from './firebase'
-import {query, collection} from 'firebase/firestore'
+import {query, collection, onSnapshot} from 'firebase/firestore'
 
 const style = {
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#EBB8DD] to-[#CAE7D3]`,
@@ -15,8 +15,25 @@ const style = {
 }
 
 function App() {
-  const [todos,setTodos] = useState(['Learn React', 'Grind LC'])
-  
+  const [todos,setTodos] = useState([] )
+  // create todo
+  // read todo from firebase
+  useEffect(()=>{
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id:doc.id})
+      })
+      setTodos(todosArr)
+    })
+    return () => unsubscribe()
+  },[])
+  // update todo in firebase
+  // delete to do
+
+
+
   return (
     <div className={style.bg}>
       <div className={style.container}>
